a1cf96fbfc60a969502da8bf85e4a9ef68e192c8
daniel-z-johnson <danzaljohnson@gmail.com>
2022-12-19 18:44:29 -0700

    add flag for back fir

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"go.uber.org/zap"
 )
 
 func main() {
+	backupFolder := flag.String("location", "~/Documents/spotifyBackup", "location of the backup folder")
+	flag.Parse()
 	logger, err := zap.NewProduction()
 	if err != nil {
 		panic(err)
 	}
 	defer logger.Sync()
 	logger.Info("Application Start")
+	logger.Info("Details", zap.String("folder", *backupFolder))
 }
