fb678c30044d85e44c3aa96673b6f0556c71059a
daniel-z-johnson <danzaljohnson@gmail.com>
2023-07-08 03:53:31 -0500

    poc that templates work

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/daniel-z-johnson/spotify-backup
 
 go 1.20
 
-require github.com/rs/zerolog v1.29.1
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/rs/zerolog v1.29.1
+)
 
 require (
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	golang.org/x/sys v0.10.0 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"github.com/daniel-z-johnson/spotify-backup/templates"
+	"github.com/daniel-z-johnson/spotify-backup/views"
+	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"net/http"
 )
 
 func main() {
@@ -13,4 +17,16 @@ func main() {
 		Logger()
 	logger.Info().
 		Msg("Application Start")
+
+	handler := chi.NewMux()
+	example, err := views.ParseFS(templates.TemplateFiles, "main.gohtml", "example.gohtml")
+	if err != nil {
+		panic("Template issue: " + err.Error())
+	}
+	handler.HandleFunc("/example", func(resp http.ResponseWriter, req *http.Request) {
+		example.Execute(resp, nil)
+	})
+	if err := http.ListenAndServe(":1117", handler); err != nil {
+		panic(err)
+	}
 }
