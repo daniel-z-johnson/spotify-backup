2eee25b1efef0488ebff8b24751e3f97b56d12a1
Daniel <danielzjohnson@sleipnir.lan>
2023-07-19 22:37:14 -0500

    logging requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/daniel-z-johnson/spotify-backup/controllers"
+	"github.com/daniel-z-johnson/spotify-backup/middleware"
 	"github.com/daniel-z-johnson/spotify-backup/templates"
 	"github.com/daniel-z-johnson/spotify-backup/views"
 	"github.com/go-chi/chi/v5"
@@ -22,6 +23,7 @@ func main() {
 	router := chi.NewMux()
 	example := views.Must(views.ParseFS(templates.TemplateFiles, "main.gohtml", "example.gohtml"))
 	staticHandler := http.FileServer(http.FS(templates.StaticFiles))
+	router.Use(middleware.Logger(logger))
 	router.Get("/example", controllers.StaticPage(example))
 	router.Handle("/static/*", staticHandler)
 	if err := http.ListenAndServe(":1117", router); err != nil {
---
+++ middleware/logger.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"github.com/rs/zerolog"
+	"net/http"
+	"time"
+)
+
+func Logger(logger zerolog.Logger) func(handler http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+
+			start := time.Now()
+			wrapped := wrapResponseWriter(w)
+			next.ServeHTTP(wrapped, r)
+			logger.Info().
+				Str("duration", time.Since(start).String()).
+				Int("code", wrapped.Status()).
+				Str("path", r.URL.Path).
+				Str("method", r.Method).
+				Msg("request info")
+		}
+
+		return http.HandlerFunc(fn)
+	}
+
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	code        int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(writer http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: writer}
+}
+
+func (rw *responseWriter) Status() int {
+	if !rw.wroteHeader {
+		rw.code = 200
+	}
+	return rw.code
+}
+func (rw *responseWriter) WriteHeader(code int) {
+	if !rw.wroteHeader {
+		rw.code = code
+		rw.ResponseWriter.WriteHeader(code)
+		rw.wroteHeader = true
+	}
+}
