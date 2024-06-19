e96e3b0ac278370139dbdc072c10115dd3ec5f19
daniel-z-johnson <danzaljohnson@gmail.com>
2024-06-19 16:14:43 -0500

    issue with wrong name for variable

--- models/sessions.go
+++ models/sessions.go
@@ -4,11 +4,22 @@ import "database/sql"
 
 type Session struct {
 	ID        int64
-	key       string
+	Key       string
 	Token     string
 	TokenHash string
 }
 
 type SessionRepo struct {
 	db *sql.DB
+}
+
+func (sr *SessionRepo) Save(s *Session) (*Session, error) {
+	row := sr.db.QueryRow(`INSERT INTO sessions (token_has, key) 
+									VALUES ($1, $2) RETURNING id`,
+		s.TokenHash, s.Key)
+	err := row.Scan(&s.ID)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
 }
