914c7313c3e38db4bfc8989f81ed5f85c2b66b72
daniel-z-johnson <danzaljohnson@gmail.com>
2024-07-27 01:36:22 -0500

    starting over

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"github.com/daniel-z-johnson/spotify-backup/conf"
-	"github.com/daniel-z-johnson/spotify-backup/models"
+
 	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 
+	"github.com/daniel-z-johnson/spotify-backup/conf"
 	"github.com/daniel-z-johnson/spotify-backup/controllers"
 	"github.com/daniel-z-johnson/spotify-backup/middleware"
+	"github.com/daniel-z-johnson/spotify-backup/models"
 	"github.com/daniel-z-johnson/spotify-backup/templates"
 	"github.com/daniel-z-johnson/spotify-backup/views"
 )
@@ -26,23 +27,15 @@ func main() {
 	flag.Parse()
 
 	config := confSetup(fileLocation)
-	db := dbSetup(config)
-	sessionRepo := &models.SessionRepo{DB: db}
+	dbSetup(config) // need to use
 
 	router := chi.NewMux()
 	example := views.Must(views.ParseFS(templates.TemplateFiles, "main.gohtml", "example.gohtml"))
-	oauthPage := views.Must(views.ParseFS(templates.TemplateFiles, "main.gohtml", "oauthpage.gohtml"))
 	staticHandler := http.FileServer(http.FS(templates.StaticFiles))
-	music := &controllers.Music{}
-	music.Templates.OAuthPage = oauthPage
-	sessionStore := middleware.SessionStore{Session: sessionRepo}
 
 	router.Use(middleware.Logger(logger))
-	router.Use(middleware.Session())
-	router.Use(sessionStore.HandleState())
 
 	router.Get("/example", controllers.StaticPage(example))
-	router.Get("/oauth", music.OauthPage)
 	router.Handle("/static/*", staticHandler)
 	if err := http.ListenAndServe(":1117", router); err != nil {
 		panic(err)
